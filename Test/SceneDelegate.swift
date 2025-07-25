f4f2898cb38fe99241d1d2360367505dc9451694
Malik <timurkaev_malik@icloud.com>
2025-07-25 11:16:51 +0300

    Initial Commit

---
+++ Test/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Test
+//
+//  Created by Malik Timurkaev on 30.06.2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        window.rootViewController = UINavigationController(rootViewController:  ViewController())
+        window.makeKeyAndVisible()
+    }
+}
---
+++ Test/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BadSecondViewController.swift
+//  Test
+//
+//  Created by Malik Timurkaev on 30.06.2025.
+//
+
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
